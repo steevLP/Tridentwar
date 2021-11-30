42e537e4cb091111a0480adc69cd32d6c238824a
BuildTools <mauricefl@outlook.de>
2021-11-30 20:33:38 +0100

    added trident handling

---
+++ src/main/java/de/steev/Tridentwar/listeners/PlayerDeathListener.java
@@ -0,0 +1,19 @@
+package de.steev.Tridentwar.listeners;
+
+import de.steev.Tridentwar.manager.GameManager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class PlayerDeathListener implements Listener {
+    private GameManager gameManager;
+
+    public PlayerDeathListener(GameManager gameManager) {
+        this.gameManager = gameManager;
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        this.gameManager.getPlayerManager().playerDeath();
+    }
+}
--- src/main/java/de/steev/Tridentwar/manager/PlayerManager.java
+++ src/main/java/de/steev/Tridentwar/manager/PlayerManager.java
@@ -8,16 +8,26 @@ import org.bukkit.inventory.ItemStack;
 
 public class PlayerManager {
     private GameManager gameManager;
+    private int alive = 0;
 
     public PlayerManager(GameManager gameManager) {
         this.gameManager = gameManager;
     }
 
-    public void giveKits() {
-        Bukkit.getOnlinePlayers().stream().filter(player -> player.getGameMode() == GameMode.SURVIVAL).forEach(this::giveKit);
-    }
-
+    public void giveKits() { Bukkit.getOnlinePlayers().stream().filter(player -> player.getGameMode() == GameMode.SURVIVAL).forEach(this::giveKit); }
     public void giveKit(Player player){
         player.getInventory().addItem(new ItemStack(Material.TRIDENT));
     }
+
+    public void playerDeath(){
+        this.alive--;
+        if(this.alive == 1) {
+            this.gameManager.setGameState(GameState.WON);
+        } else if(this.alive == 0) {
+            this.gameManager.setGameState(GameState.ABORTING);
+        }
+    }
+
+    public int getAlive() { return alive; }
+    public void setAlive(int alive) { this.alive = alive; }
 }
